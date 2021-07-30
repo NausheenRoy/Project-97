36fd7c84006c1cf2eeb3f64150015967b6b69114
NausheenRoy <75558008+NausheenRoy@users.noreply.github.com>
2021-07-30 12:27:07 +0530

    Add files via upload

---
+++ NumberGuessing.py
@@ -0,0 +1,21 @@
+import random
+chancec = 0
+number = random.randit(1-9)
+
+while chances<5:
+    if guess = number
+    print("Congratulation!! You got it right")
+    break
+
+  if guess > number:
+        print("Your Number Is Too High")
+        
+    if guess < number:
+        print("Your Number Is Too Low")
+
+
+    chances = chances +1
+
+if chances > 5:
+    print("Oops Try Again You Failed Dumbo")
+
