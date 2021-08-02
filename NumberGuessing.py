f2fd66d7d9c9d149b5cc53a43ab83fd37ae32f2f
NausheenRoy <75558008+NausheenRoy@users.noreply.github.com>
2021-08-02 12:53:39 +0530

    Update NumberGuessing.py

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -1,13 +1,16 @@
+  
 import random
-chancec = 0
-number = random.randit(1-9)
+chances = 0
+number = random.randint(1,9)
+print("Number Guessing Game")
+while chances < 5:
+    guess = int(input("Enter Your Guess Number: "))
+    
+    if guess == number:
+        print("Congrats You Won!!!")
+        break
 
-while chances<5:
-    if guess = number
-    print("Congratulation!! You got it right")
-    break
-
-  if guess > number:
+    if guess > number:
         print("Your Number Is Too High")
         
     if guess < number:
